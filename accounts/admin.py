91fecc3bfa792f12ed5a236f4fcd302b2685b48c
00022067 <00022067wiut@gmail.com>
2025-12-05 17:55:02 +0500

    connected to the database

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,5 +1,30 @@
 from django.contrib import admin
-from . import models
+from bookings.models import Car, Booking, Service
 
-# Register account models here
-admin.site.register(models.ExampleProfile)
+
+@admin.register(Car)
+class CarAdmin(admin.ModelAdmin):
+    list_display = ('brand', 'model', 'year', 'price_per_day', 'user', 'status')
+    search_fields = ('brand', 'model')
+    list_filter = ('status', 'year')
+
+
+@admin.register(Booking)
+class BookingAdmin(admin.ModelAdmin):
+    list_display = (
+        'car',
+        'customer',
+        'service',
+        'start_date',
+        'end_date',
+        'total_cost',
+        'status'
+    )
+    search_fields = ('customer__username', 'car__brand', 'car__model')
+    list_filter = ('status', 'start_date')
+
+
+@admin.register(Service)
+class ServiceAdmin(admin.ModelAdmin):
+    list_display = ('name', 'duration', 'price')
+    search_fields = ('name',)
--- mysite/settings.py
+++ mysite/settings.py
@@ -83,13 +83,22 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'rentcar_db',
+        'USER': 'postgres',
+        'PASSWORD': 'diyorbek',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
