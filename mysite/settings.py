c23d3dd4ab3e79be1caa5e0543c3062603968a19
00022067 <00022067wiut@gmail.com>
2025-12-05 22:33:40 +0500

    ui added for api

--- mysite/settings.py
+++ mysite/settings.py
@@ -60,6 +60,18 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'mysite.urls'
 
+REST_FRAMEWORK = {
+    'DEFAULT_RENDERER_CLASSES': [
+        'rest_framework.renderers.JSONRenderer',
+        'rest_framework.renderers.BrowsableAPIRenderer',
+    ],
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
+    ]
+}
+
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
